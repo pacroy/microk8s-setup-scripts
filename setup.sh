10b32468d06cc89ff0d0158c831c5b6bacaf0a82
Par <pacroy@gmail.com>
2021-05-21 21:40:11 +0700

    Add configure remote access

--- setup.sh
+++ setup.sh
@@ -20,3 +20,9 @@ microk8s kubectl get services
 
 # Enable dns and storage addons
 microk8s enable dns storage
+
+# Configure remote access by adding machine public hostname and IP address
+nano /var/snap/microk8s/current/certs/csr.conf.template
+
+# Export kubeconfig file
+microk8s config > ~/admin.config
