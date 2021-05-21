5f34bc8b0b7669c077dc0579f31bdec047982ec5
Par <pacroy@gmail.com>
2021-05-21 21:28:32 +0700

    Add configure permission

--- setup.sh
+++ setup.sh
@@ -7,3 +7,9 @@ sudo apt-get update && sudo apt-get upgrade -y
 
 # Install microk8s
 sudo snap install microk8s --classic --channel=1.21
+
+# Configure permission
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+newgrp microk8s
+microk8s status --wait-ready
