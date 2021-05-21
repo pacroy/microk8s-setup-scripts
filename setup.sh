80d5ee1c045c00bdc8bcd03bef2225e100046d71
Par <pacroy@gmail.com>
2021-05-21 21:29:54 +0700

    Add check nodes/services + enable dns storage

--- setup.sh
+++ setup.sh
@@ -13,3 +13,10 @@ sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
 newgrp microk8s
 microk8s status --wait-ready
+
+# Check nodes and services
+microk8s kubectl get nodes
+microk8s kubectl get services
+
+# Enable dns and storage addons
+microk8s enable dns storage
