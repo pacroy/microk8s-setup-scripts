257b8fea3d8e77dc61686a6fdaaffd47c8cc94da
Par <pacroy@gmail.com>
2021-09-17 16:23:30 +0700

    Add setup-email.sh

---
+++ setup-email.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+
+# Install bsd-mailx and postfix
+sudo apt install -y bsd-mailx postfix libsasl2-modules
+
+# Configure SMTP Relay
+sudo nano /etc/postfix/main.cf
+
+# Create SMTP passwd File
+sudo nano /etc/postfix/sasl_passwd
+
+# Create Hash DB
+sudo postmap /etc/postfix/sasl_passwd
+sudo chmod 0600 /etc/postfix/sasl_passwd /etc/postfix/sasl_passwd.db
+
+# Restart postfix
+sudo systemctl restart postfix
+
+# Test Sending Email
+printf "=== Test Sending Email ===\n"
+printf "Sender Email  : " && read sender
+printf "Receiver Email: " && read receiver
+echo "this is a test email." | mailx -r "$sender" -s "hello" "$receiver"
